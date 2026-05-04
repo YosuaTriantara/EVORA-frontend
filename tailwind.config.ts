0b475ee0669c9b4ef211ef99c32fb3606ad0ce32
YosuaTriantara <yosuatriantara@gmail.com>
2026-05-04 23:30:22 +0800

    add: tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1400px",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
